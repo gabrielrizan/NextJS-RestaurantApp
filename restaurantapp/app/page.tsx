5b057f1422f41b2f943f3b1d79e5c57e0dc49018
Gabriel <rizangabriel3@gmail.com>
2023-07-08 18:28:13 +0300

    Added Postgres database integration using prisma

--- restaurantapp/app/page.tsx
+++ restaurantapp/app/page.tsx
@@ -1,18 +1,24 @@
 import { Inter } from "@next/font/google";
 import Header from "./components/Header";
 import Restaurantcard from "./components/Restaurantcard";
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   
-        <main>
-          <Header />
+    <>
+      <Head>
+        <title>Home Page</title>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
+      <main>
+        <Header />
+        <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+          <Restaurantcard />
+        </div>
+      </main>
 
-          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            <Restaurantcard />
-          </div>
-        </main>
-     
+    </>
   );
 }
+ // 7oy0oXshh4Vrpawk
