2a2abcff2e34134032c96d7ad361ac90634a0eaa
Gabriel <rizangabriel3@gmail.com>
2023-07-08 16:35:55 +0300

    cleared up the code, made it more dry

--- restaurantapp/app/restaurant/[slug]/components/Images.tsx
+++ restaurantapp/app/restaurant/[slug]/components/Images.tsx
@@ -1,39 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Images = () => {
   return (
-    <div>
-    <h1 className="font-bold text-3xl mt-10 mb-7 borber-b pb-5">
-      What 100 people are saying
-    </h1>
-    <div>
-      {/* REVIEW CARD */}
-      <div className="border-b pb-7 mb-7">
-        <div className="flex">
-          <div className="w-1/6 flex flex-col items-center">
-            <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
-              <h2 className="text-white text-2xl">MJ</h2>
-            </div>
-            <p className="text-center">Micheal Jordan</p>
-          </div>
-          <div className="ml-10 w-5/6">
-            <div className="flex items-center">
-              <div className="flex mr-5">*****</div>
-            </div>
-            <div className="mt-5">
-              <p className="text-lg font-light">
-                Laurie was on top of everything! Slow night due to the
-                snow storm so it worked in our favor to have more one on
-                one with the staff. Delicious and well worth the money.
-              </p>
-            </div>
-          </div>
-        </div>
+    <>
+      <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">5 photos</h1>
+      <div className="flex flex-wrap">
+        <img
+          className="w-56 h-44 mr-1 mb-1"
+          src="https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpg"
+          alt=""
+        />
+        <img
+          className="w-56 h-44 mr-1 mb-1"
+          src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701450.jpg"
+          alt=""
+        />
+        <img
+          className="w-56 h-44 mr-1 mb-1"
+          src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701452.jpg"
+          alt=""
+        />
+        <img
+          className="w-56 h-44 mr-1 mb-1"
+          src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701453.jpg"
+          alt=""
+        />
+        <img
+          className="w-56 h-44 mr-1 mb-1"
+          src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701454.jpg"
+          alt=""
+        />
       </div>
-      {/* REVIEW CARD */}
-    </div>
-  </div>
-  )
-}
+      
+    </>
+  );
+};
 
-export default Images
+export default Images;
--- restaurantapp/app/search/page.tsx
+++ restaurantapp/app/search/page.tsx
@@ -1,4 +1,3 @@
-
 import Header from "./components/Header";
 import Searchsidebar from "./components/Searchsidebar";
 import Restaurantcard from "./components/Restaurantcard";
@@ -6,12 +5,10 @@ import Restaurantcard from "./components/Restaurantcard";
 export default function Search() {
   return (
     <>
-      {/* HEADER */}
+      {" "}
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        {/* SEARCH SIDE BAR */}
         <Searchsidebar />
-        {/* SEARCH SIDE BAR */}
         <div className="w-5/6">
           <Restaurantcard />
         </div>
